e8c32e7aed85fa62d254d8dfd896be3ccd2e3514
Manamrit Singh <manamritsingh426@gmail.com>
2023-09-02 03:16:57 +0530

    dynamic use explained

---
+++ dynamic.dart
@@ -0,0 +1,14 @@
+// variables and dynamic variables
+// inline is non dynamic
+
+void main() {
+  var name = "Singh";
+
+  //name = 7; will throw error as it should
+
+  var section;
+
+  section = "D";
+
+  section = 7; // wont give error as section is a dynamic variable
+}
