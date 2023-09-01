456d8a09f2aac32f79a8800ccc1cab1bb8afe63d
Manamrit Singh <manamritsingh426@gmail.com>
2023-09-02 03:26:17 +0530

    functions and class

---
+++ func.dart
@@ -0,0 +1,14 @@
+// make a function
+// also will elaborate the class a bit more
+
+void main() {
+  var myc = myclass();
+
+  myc.printname("hello"); // will print ofc.
+}
+
+class myclass {
+  void printname(String nam) {
+    print(nam);
+  }
+}
