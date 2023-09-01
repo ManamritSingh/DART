58c86e44263f1f9e6bfb3828cd00e29c3f47caa1
Manamrit Singh <manamritsingh426@gmail.com>
2023-09-02 02:58:43 +0530

    class basics

---
+++ class.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('hi');
+
+  var manam = new Human(); // may not write new in newer versions
+  // created a class object
+}
+
+class Human {
+  Human(); //default constructor
+}
