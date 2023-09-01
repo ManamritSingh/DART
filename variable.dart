810e571fd241a07843ed5d5105bfd0d71e16dddb
Manamrit Singh <manamritsingh426@gmail.com>
2023-09-02 03:08:10 +0530

    types of variables

---
+++ variable.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  //int a;
+  //print(a); wil throw error as by default its null
+  int? b;
+  print(b); // this prints null
+
+  BigInt c;
+  c = BigInt.parse('657859043583024');
+  // yeah its a bit wierd and different
+
+  double percent = 66.56;
+  num perc = 66.78;
+
+  bool islogin = false;
+
+  String name = 'manam';
+}
