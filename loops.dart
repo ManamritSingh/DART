36a821205d7afb33ef5b78b27630f3d1a97a58d2
Manamrit Singh <manamritsingh426@gmail.com>
2023-09-02 20:20:47 +0530

    loops in dart

---
+++ loops.dart
@@ -0,0 +1,15 @@
+void main() {
+  for (var i = 0; i < 3; i++) {
+    print('u completed dart tutorial!!');
+  }
+  var j = 0;
+  while (j < 3) {
+    print('flutter next ->');
+    j++;
+  }
+  var k = 0;
+  do {
+    print("All the best !!");
+    k++;
+  } while (k < 3);
+}
