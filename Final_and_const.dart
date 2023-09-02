68a3573ba4ed9d2f9c15e19557b4f89a5279f580
Manamrit Singh <manamritsingh426@gmail.com>
2023-09-02 20:11:56 +0530

    finals and consts

---
+++ Final_and_const.dart
@@ -0,0 +1,15 @@
+// code to understand final and const
+
+void main() {
+  final Cand_name;
+  Cand_name = 'Singh';
+
+  // Cand_name = 'manam'; cant reassign
+
+  const classr = "A";
+  //classr = 8; reassign throws error
+
+  // incase of list
+  // final array can have changes but reassign possible
+  // const array can not be modified at any cost
+}
